aba8e60e8733263cb1b6c9e4e671507c8fc46a28
Janice <janice0975751227@gmail.com>
2025-01-18 22:11:28 +0800

    flip card individual

--- my-app/src/components/Container.jsx
+++ my-app/src/components/Container.jsx
@@ -3,27 +3,33 @@ import Card from "./Card"
 import Score from "./Score"
 import Timer from "./Timer"
 
+const OPTIONS = [
+    {value: 1, visibility: false},
+    {value: 2, visibility: false },
+    {value: 1, visibility: false },
+    {value: 2, visibility: false }]
+
 function Container(props){
 
-    const options = [1,2,3]
-    const[visible, setVisible]=useState(false)
+    const[options, setOptions] = useState(OPTIONS)
 
-    let flip = () => {
-        setVisible(!visible)
+    let flip = (index) => {
+        var newOption = JSON.parse(JSON.stringify(options)) //deep copy, 把options內的東西stringify然後parse, 傳給newOption
+        newOption[index].visibility = !newOption[index].visibility //如果本來有顯示內容->隱藏; 如果原本就隱藏->顯示內容
+        setOptions(newOption)
     }
 
     return (
         <>
-            {options.map((i) => 
+            {options.map((i, index) => 
                 <Card
-                    visible={visible} 
-                    onClick={flip} 
-                    content={i}
+                    visible={i.visibility} 
+                    onClick={() => flip(index)} 
+                    content={i.value}
                 />
             )}
         </>
     )
 }
-    
 
 export default Container
